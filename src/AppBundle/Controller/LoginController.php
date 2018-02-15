d22d32d5692308f221b9c4f60e781ae2af257145
Tantol <tomando5218@gmail.com>
2018-02-15 21:10:05 +0100

    login and logout functionality

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        
+        $errors = $authenticationUtils->getLastAuthenticationError();
+        
+        $lastUserName = $authenticationUtils->getLastUsername();
+        
+        return $this->render('Login/login.html.twig', array(
+            'errors' => $errors,
+            'username' => $lastUserName,
+        ));
+    }
+    
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        
+        
+    }
+
+}
